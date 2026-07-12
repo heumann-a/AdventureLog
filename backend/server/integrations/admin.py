e424b93d716452c092532092b1c2f6bc1b13496c
heuman-a <heuman-a>
2026-07-12 15:56:46 +0200

    Add toggle for auto-update expiration of garmin login

--- backend/server/integrations/admin.py
+++ backend/server/integrations/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from allauth.account.decorators import secure_admin_login
 
-from .models import ImmichIntegration, StravaToken, GarminToken, WandererIntegration
+from .models import ImmichIntegration, StravaToken, GarminIntegration, WandererIntegration
 
 admin.autodiscover()
 admin.site.login = secure_admin_login(admin.site.login)
 
 admin.site.register(ImmichIntegration)
 admin.site.register(StravaToken)
-admin.site.register(GarminToken)
+admin.site.register(GarminIntegration)
 admin.site.register(WandererIntegration)
